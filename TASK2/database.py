6018986fe0702fa726c05ac3ff572bd20d020f3a
Harish S <88659846+Harish296@users.noreply.github.com>
2026-01-17 09:37:10 +0530

    Update database.py

--- TASK2/database.py
+++ TASK2/database.py
@@ -8,11 +8,12 @@ cursor.execute("CREATE TABLE users (id INTEGER, name TEXT, device TEXT)")
 
 cursor.execute("INSERT INTO users VALUES (1, 'Admin', 'Legacy System')")
 cursor.execute("INSERT INTO users VALUES (2, 'Bob', 'Lenovo Legion')")
-cursor.execute("INSERT INTO users VALUES (3, 'Peter', 'Mac')")
+cursor.execute("INSERT INTO users VALUES (3, 'Mark', 'Mac')")
 cursor.execute("INSERT INTO users VALUES (4, 'Peter', 'Dell G15')")
 cursor.execute("INSERT INTO users VALUES (5, 'John', 'Acer Nitro')")
 cursor.execute("INSERT INTO users VALUES (6, 'Alice', 'Hp Omen')")
 
 conn.commit()
 conn.close()
+
 
